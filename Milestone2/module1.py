45c2bbbed9762c182cdbc7f29f1f4d0ee539e3cf
vgomes-p <vigomes-@student.42sp.org.br>
2026-07-19 20:39:25 -0300

    wip: module1 tester

---
+++ Milestone2/module1.py
@@ -0,0 +1,260 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    module1.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vigomes- <vigomes-@student.42sp.org.br>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/07/12 14:55:15 by vigomes-          #+#    #+#              #
+#    Updated: 2026/07/19 20:37:00 by vigomes-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from unittest.mock import patch
+from io import StringIO
+from contextlib import redirect_stdout
+import time as tm
+import argparse
+from utils_m0 import list_of_flowers0, list_of_flowers1, list_of_plants0, list_of_plants1, list_of_trees, list_of_vegetables
+from utils_m0 import clear, printnl, error_message, biggest_name
+from utils_m0 import DEFAULT, CYAN,PINK, GREEN, RED
+from utils_m0 import TEXTS, MID_IDS, END_IDS
+from utils_m0 import f0e, f1e, f2e
+
+
+auto_final_text_s = "CONGRATS, ALL RAN SUCESSFULLY!"
+auto_final_text_f = "SOMETHING IS WRONG, REVIEW YOUR CODE!"
+
+
+def build_args(parameter: list) -> list:
+    args = [
+        parameter["name"],
+        parameter["height"]
+    ]
+    if "plant_age" in parameter:
+        args.append(parameter["plant_age"])
+    if "grpd" in parameter:
+        args.append(parameter["grpd"])
+    return args
+
+
+def exec_no_class(function, function_name: str, parameters: dict, in_params: list, expected: dict, texts: dict, space_time: int = 1, view: bool=False, time: float=0.5) -> tuple[int, int]:
+    i = 0
+    stats = 0
+    init_text = texts.get("init_text")
+    end_text = texts.get("end_text")
+    print(f"{CYAN}\n{function_name}:{' ' * space_time}{DEFAULT}", end=('\n' if view else ''), flush=True)
+    for parameter in parameters.values():
+        tm.sleep(time)
+        expect = expected[i]
+        param1 = parameter[str(in_params[0])]
+        param2 = int(parameter[str(in_params[1])])
+        param3 = int(parameter[str(in_params[2])])
+        buffer = StringIO()
+        try:
+            with redirect_stdout(buffer):
+                print(init_text if init_text else "", end = '')
+                function(param1, param2, param3)
+                print(end_text if end_text else "", end = '')
+            output = buffer.getvalue()
+            if output == expect:
+                print('✅', end='', flush=True)
+            else:
+                print('❌', end='', flush=True)
+                stats = 1
+            if view:
+                print(f"\nEXPECTED:\n{expect}\nOUTPUT:\n{output}")
+        except Exception as e:
+            error_message(f"Serious error on running {function_name}(): {e}")
+            return 1, 1
+        i += 1;
+    printnl()
+    return 0, stats
+
+
+def exec_mod(fclass, id: int, modules: dict, texts: dict, args: list, assign_name: str) -> str:
+    init_text = texts.get("init_text")
+    mid_text = texts.get("mid_text")
+    end_text = texts.get("end_text")
+    
+    buffer = StringIO()
+    i = 0
+    with redirect_stdout(buffer):
+        try:
+            obj = fclass(*args)
+            for module in modules:
+                if i == 0:
+                    print(init_text if init_text else "", end='')
+                if i == MID_IDS[id]:
+                    print(mid_text if mid_text else "", end='')
+                if i == END_IDS[id]:
+                    print(end_text if end_text else "", end='')
+                if id == 2 and i == 1:
+                    getattr(obj, module)(7)
+                else:
+                    getattr(obj, module)()
+                i += 1
+        except Exception as e:
+            return e, 1
+    return buffer.getvalue()
+
+
+def exec_class(fclass, id: int, modules: dict, assign_name: str, parameters: dict, expected: dict, texts: dict, space_time: int = 1, view: bool=False, time: float=0.5) -> tuple[int, int]:
+    i = 0
+    stats = 0
+    print(f"{CYAN}\n{assign_name}:{' ' * space_time}{DEFAULT}", end=('\n' if view else ''), flush=True)
+    for i, parameter in parameters.items():
+        tm.sleep(time)
+        buffer = StringIO()
+        try:
+            args = build_args(parameter)
+            output = exec_mod(fclass, id, modules, texts, args, assign_name)
+            if output == expected[i]:
+                print('✅', end='', flush=True)
+            else:
+                print('❌', end='', flush=True)
+                stats = 1
+            if view:
+                print(f"\nEXPECTED:\n{expected[i]}\nOUTPUT:\n{output}")
+        except Exception as e:
+            error_message(f"Serious error on running {assign_name}: {e}")
+            return 1, 1
+        i += 1;
+    printnl()
+    return 0, stats
+
+
+def exec_tests(view: bool, time: float) -> int:
+    try:
+        from ex0.ft_garden_intro import ft_my_garden
+        from ex1.ft_garden_data import Plant as Pl1
+        from ex2.ft_plant_growth import Plant as Pl2
+        from ex3.ft_plant_factory import Plant as Pl3
+        from ex4.ft_garden_security import Plant as Pl4
+        from ex5.ft_plant_types import Flower
+        from ex5.ft_plant_types import Tree
+        from ex5.ft_plant_types import Vegetables
+        up_coming = "up_coming"
+    except ImportError as e:
+        error_message(e)
+    finally:
+        funcs = {
+            "f0": ft_my_garden,
+            "f1": Pl1,
+            "f2": Pl2,
+            "f3": Pl3,
+            "f4": Pl4,
+            "f5": Flower,
+            "f6": Tree,
+            "f7": Vegetables,
+            "f8": up_coming,
+        }
+
+        funcs_names = {
+            "f0": "ft_my_garden()",
+            "f1": "Gerden Data",
+            "f2": "Plant Growth",
+            "f3": "Plant Factory",
+            "f4": "Garden Security",
+            "f5": "Plant Type: Flower",
+            "f6": "Plant Type: Tree",
+            "f7": "Plant Type: Vegetables",
+            "f8": "ft_seed_inventory",
+        }
+
+        funcs_params = {
+            "f0": list_of_flowers0,
+            "f1": list_of_plants0,
+            "f2": list_of_plants1,
+            "f3": list_of_plants1,
+            "f4": list_of_plants1,
+            "f5": list_of_flowers1,
+            "f6": list_of_trees,
+            "f7": list_of_vegetables,
+        }
+
+        in_params0 = ["name", "height", "age"]
+
+        class_modules = {
+            "f0": ["show"],
+            "f1": ["show"],
+            "f2": ["show", "grow"],
+            "f3": ["show", "grow"],
+            "f4": ["show", "grow"],
+            "f5": ["show", "grow"],
+            "f6": ["show", "grow"],
+            "f7": ["show", "grow"],
+        }
+
+        ret_expect = {
+            "f0": f0e,
+            "f1": f1e,
+            "f2": f2e,
+            "f3": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            "f4": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            "f5": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            "f6": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            "f7": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+        }
+
+    ret = 0
+    i = 0;
+    clear()
+    checker = 0
+    print(f"{PINK}RUNNING TESTS FOR PYTHON MODULE 1!{DEFAULT}")
+    print(f"{PINK}-----------------------------------------{DEFAULT}", end='')
+    tm.sleep(time)
+    while ret == 0:
+        if i > 8:
+            break
+        to_get = f"f{i}"
+        to_run = funcs.get(to_get)
+        fun_name = funcs_names.get(to_get)
+        params = funcs_params.get(to_get)
+        params_expect = ret_expect.get(to_get)
+        modules = class_modules.get(to_get)
+        texts = TEXTS.get(to_get)
+        biggest_name_len = biggest_name(funcs_names)
+        if i == 0:
+            in_params = in_params0
+        if len(fun_name) != biggest_name_len:
+            space_times = biggest_name_len - len(fun_name) + 1
+        else:
+            space_times = 1
+        tm.sleep(time)
+        if i == 0:
+            ret, stats = exec_no_class(to_run, fun_name, params, in_params, params_expect, texts, space_times, view, time)
+            checker += stats
+        elif i == 8:
+            pass
+            # ret, stats = exec_params(to_run, fun_name, params, params_expect, space_times, view, time)
+            # checker += stats
+        else:
+            ret, stats = exec_class(to_run, i, modules, fun_name, params, params_expect, texts, space_times, view, time)
+            checker += stats
+            if i == 2:
+                break
+        i += 1
+    if ret == 0:
+        print(f"\n{PINK}-----------------------------------------{DEFAULT}")
+        print(f"{GREEN if checker == 0 else RED}{auto_final_text_s if checker == 0 else auto_final_text_f}{DEFAULT}")
+    return ret
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(description="Tester for Python Module 0 from Ecole 42")
+    parser.add_argument("--visual", action="store_true", help="Print functions returns")
+    parser.add_argument("--fast", action="store_true", help="run tests faster")
+
+    view = False
+    time = 0.5
+    args = parser.parse_args()
+    if args.visual:
+        view = True
+    elif args.fast:
+        time = 0.05
+    return exec_tests(view, time)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Milestone2/utils_m0.py
@@ -0,0 +1,155 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    utils_m0.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vigomes- <vigomes-@student.42sp.org.br>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/07/19 20:01:16 by vigomes-          #+#    #+#              #
+#    Updated: 2026/07/19 20:35:47 by vigomes-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import os
+import subprocess
+
+DEFAULT = "\033[m"
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+YLOW = "\033[1;33m"
+PINK = "\033[1;35m"
+CYAN = "\033[1;36m"
+INVERT = "\033[1;4;7;97m"
+BOLD = "\033[1m"
+
+list_of_flowers0 = {
+    0: {"name": "Rose", "height": 25, "age": 30},
+    1: {"name": "Sun Flower", "height": 50, "age": 45},
+    2: {"name": "Orchid", "height": 15, "age": 420}
+    }
+
+list_of_flowers1 = {
+    0: {"name": "Rose", "height": 25.0, "age": 30, "grpd": 0.86, "color": "White"}
+}
+
+list_of_plants0 = {
+    0: {"name": "Rose", "height": 25, "plant_age": 30},
+    1: {"name": "Sunflower", "height": 80, "plant_age": 45},
+    2: {"name": "Cactus", "height": 15, "plant_age": 120},
+    3: {"name": "Olive", "height": 150, "plant_age": 365},
+    4: {"name": "Venus", "height": 9, "plant_age": 1},
+    5: {"name": "whatever", "height": 0, "plant_age": 0},
+}
+
+list_of_plants1 = {
+    0: {"name": "Rose", "height": 25, "plant_age": 30, "grpd": 0.86},
+    1: {"name": "Sunflower", "height": 80, "plant_age": 45, "grpd": 0.9},
+    2: {"name": "Cactus", "height": -15, "plant_age": 120, "grpd": 0.15},
+    3: {"name": "Olive", "height": 150, "plant_age": -365, "grpd": 0.02},
+    4: {"name": "Venus", "height": 9, "plant_age": 1, "grpd": 0.5},
+    5: {"name": "whatever", "height": 0, "plant_age": 1, "grpd": 2.0},
+}
+
+list_of_trees = {
+    0: {"name": "Oak", "height": 200.0, "age": 1095, "grpd": 10.0, "trunk_diameter": 5.0}
+}
+
+list_of_vegetables = {
+    0: {"name": "Tomato", "height": 5.0, "age": 10, "grpd": 2.1, "season": "April", "nutri": 1}
+}
+
+TEXTS = {
+    "f0": {"init_text": "=== Welcome to My Garden ===\n", "mid_text": None, "end_text": "=== End of Program ===\n"},
+    "f1": {"init_text": "=== Garden Plant Registry ===\n", "mid_text": None, "end_text": None},
+    "f2": {"init_text": "=== Day 1 ===\n", "mid_text": "=== Day 7 ===\n", "end_text": None},
+    "f3": {"init_text": "", "mid_text": "", "end_text": ""},
+    "f4": {"init_text": "", "mid_text": "", "end_text": ""},
+    "f5": {"init_text": "", "mid_text": "", "end_text": ""},
+    "f6": {"init_text": "", "mid_text": "", "end_text": ""},
+    "f7": {"init_text": "", "mid_text": "", "end_text": ""},
+}
+
+MID_IDS = {
+    0: -1,
+    1: 1,
+    2: 1,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+}
+
+END_IDS = {
+    0: 1,
+    1: -1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+}
+
+f0e = {
+    0: "=== Welcome to My Garden ===\nPlant: Rose\nHeight: 25cm\nAge: 30 days\n=== End of Program ===\n",
+    1: "=== Welcome to My Garden ===\nPlant: Sun Flower\nHeight: 50cm\nAge: 45 days\n=== End of Program ===\n",
+    2: "=== Welcome to My Garden ===\nPlant: Orchid\nHeight: 15cm\nAge: 420 days\n=== End of Program ===\n",
+    }
+
+f1e = {
+    0: "=== Garden Plant Registry ===\nRose: 25.0cm, 30 days old\n",
+    1: "=== Garden Plant Registry ===\nSunflower: 80.0cm, 45 days old\n",
+    2: "=== Garden Plant Registry ===\nCactus: 15.0cm, 120 days old\n",
+    3: "=== Garden Plant Registry ===\nOlive: 150.0cm, 365 days old\n",
+    4: "=== Garden Plant Registry ===\nVenus: 9.0cm, 1 days old\n",
+    5: "=== Garden Plant Registry ===\nwhatever: 0.0cm, 0 days old\n"
+    }
+
+# f2e = {
+#     0: "=== Day 1 ===\nRose: 25.0cm, 30 days old\n=== Day 7 ===\nRose: 31.0cm, 37 days old\nGrowth this week: +6.02cm\n",
+#     1: "=== Day 1 ===\nSunflower: 80.0cm, 45 days old\n=== Day 7 ===\nSunflower: 86.3cm, 52 days old\nGrowth this week: +6.30cm\n",
+#     2: "Cactus: 15.0cm, 120 days old\n=== Day 7 ===\nCactus: 16.1cm, 127 days old\nGrowth this week: +1.05cm\n",
+#     3: "=== Day 1 ===\nOlive: 150.0cm, 365 days old\n=== Day 7 ===\nOlive: 150.1cm, 372 days old\nGrowth this week: +0.14cm\n",
+#     4: "=== Day 1 ===\nVenus: 9.0cm, 1 days old\n=== Day 7 ===\nVenus: 12.5cm, 8 days old\nGrowth this week: +3.50cm\n",
+#     5: "=== Day 1 ===\nwhatever: 0.0cm, 1 days old\n=== Day 7 ===\nGrowth this week: +14.00cm\n"
+#     }
+
+f2e = {
+    0: "=== Day 1 ===\nRose: 25.0cm, 30 days old\n=== Day 7 ===\nRose: 31.0cm, 37 days old\n",
+    1: "=== Day 1 ===\nSunflower: 80.0cm, 45 days old\n=== Day 7 ===\nSunflower: 86.3cm, 52 days old\n",
+    2: "Cactus: 15.0cm, 120 days old\n=== Day 7 ===\nCactus: 16.1cm, 127 days old\n",
+    3: "=== Day 1 ===\nOlive: 150.0cm, 365 days old\n=== Day 7 ===\nOlive: 150.1cm, 372 days old\n",
+    4: "=== Day 1 ===\nVenus: 9.0cm, 1 days old\n=== Day 7 ===\nVenus: 12.5cm, 8 days old\n",
+    5: "=== Day 1 ===\nwhatever: 0.0cm, 1 days old\n=== Day 7 ===\nwhatever: 14.0cm, 8 days old\n"
+    }
+
+
+def clear() -> None:
+    cmd = []
+    os_name = os.name
+    if os_name == "nt":
+        cmd.append("cls")
+    else:
+        cmd.append("clear")
+    try:
+        subprocess.run(cmd)
+    except Exception as e:
+        pass
+
+
+def printnl(times: int=0) -> None:
+    print("\n" * times, end="")
+
+
+def error_message(error: str, x: bool=True) -> None:
+    if (x):
+        print("❌", end='')
+    print(f"{RED} {error}{DEFAULT}")
+
+
+def biggest_name(dict_names: dict) -> int:
+    biggest = 0
+    for code in dict_names:
+        name_len = len(dict_names[code])
+        if name_len >= biggest:
+            biggest = name_len
+    return biggest
