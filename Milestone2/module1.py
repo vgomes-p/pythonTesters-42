ff6761bad356e407e234f37a30a237a7a624f42a
vgomes-p <vigomes-@student.42sp.org.br>
2026-07-20 20:54:22 -0300

    wip: Milestone2/module1-ex3 tester

--- Milestone2/module1.py
+++ Milestone2/module1.py
@@ -6,20 +6,39 @@
 #    By: vigomes- <vigomes-@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/12 14:55:15 by vigomes-          #+#    #+#              #
-#    Updated: 2026/07/19 20:37:00 by vigomes-         ###   ########.fr        #
+#    Updated: 2026/07/20 20:52:01 by vigomes-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+# YOU MIGHT WANT TO READ THIS
+"""
+First of all, i'm also asking me what the fuck is this script.
+
+Second of all, it all started beautifully, but after ex1 I notice that people
+will implement the assignments differently and it was in that moment that i knew
+I fucked up. That's how what should be simple became a monster (because I refuse
+to ask AI to do everything, I want to learn by making the worst code I can and
+do it better the next time.)
+
+I don't recomend you to modify this code at any line, it might break and you
+probably will want to fix it and spend a long time on it. You better go learn
+something new.
+
+As a warn, this is the amount of days spend on this code (fixing, improving or
+implementing something): 3 days
+"""
 
 from unittest.mock import patch
 from io import StringIO
 from contextlib import redirect_stdout
 import time as tm
 import argparse
-from utils_m0 import list_of_flowers0, list_of_flowers1, list_of_plants0, list_of_plants1, list_of_trees, list_of_vegetables
+from utils_m0 import list_of_flowers0, list_of_flowers1, list_of_plants0, list_of_plants1, list_of_plants2, list_of_trees, list_of_vegetables
 from utils_m0 import clear, printnl, error_message, biggest_name
-from utils_m0 import DEFAULT, CYAN,PINK, GREEN, RED
+from utils_m0 import DEFAULT, RED, GREEN,  PINK, CYAN
 from utils_m0 import TEXTS, MID_IDS, END_IDS
-from utils_m0 import f0e, f1e, f2e
+from utils_m0 import f0e, f1e, f2e, f3e1, f3e2, f3e3
+import pdb
 
 
 auto_final_text_s = "CONGRATS, ALL RAN SUCESSFULLY!"
@@ -73,9 +92,11 @@ def exec_no_class(function, function_name: str, parameters: dict, in_params: lis
 
 
 def exec_mod(fclass, id: int, modules: dict, texts: dict, args: list, assign_name: str) -> str:
+    # pdb.set_trace()
     init_text = texts.get("init_text")
     mid_text = texts.get("mid_text")
     end_text = texts.get("end_text")
+    temp = 0
     
     buffer = StringIO()
     i = 0
@@ -83,6 +104,7 @@ def exec_mod(fclass, id: int, modules: dict, texts: dict, args: list, assign_nam
         try:
             obj = fclass(*args)
             for module in modules:
+                # print(f"DEBUG: module: {module} | id: {id} | i: {i} | MID_IDS: {MID_IDS[id]} | END_IDS: {END_IDS[id]}")
                 if i == 0:
                     print(init_text if init_text else "", end='')
                 if i == MID_IDS[id]:
@@ -90,36 +112,105 @@ def exec_mod(fclass, id: int, modules: dict, texts: dict, args: list, assign_nam
                 if i == END_IDS[id]:
                     print(end_text if end_text else "", end='')
                 if id == 2 and i == 1:
-                    getattr(obj, module)(7)
+                    temp = getattr(obj, module)(7)
                 else:
                     getattr(obj, module)()
+                    if id == 2 and i == 2:
+                        print(f"Growth this week: +{temp:.2f}cm")
                 i += 1
         except Exception as e:
-            return e, 1
+            error_message(f"Serious error on running {assign_name}: {e}")
+            return None
     return buffer.getvalue()
+
+
+def exec_special3(fclass, id: int, modules: dict, assign_name: str, parameters: dict, expected: dict, view: bool=False, time: float=0.5) -> int:
+    expected1 = expected.get(1)
+    expected2 = expected.get(2)
+    expected3 = expected.get(3)
+    stats = 0
+    i = 0
+
+    for p in parameters:
+        plant = parameters[p]
+        buffer1 = StringIO()
+        with redirect_stdout(buffer1):
+            try:
+                plants = fclass(plant["name"], plant["height"], plant["age"], plant["grpd"])
+                try:
+                    print(f"Total of plants: {plants.total_plants}")
+                except:
+                    print("no class variable like total_plant")
+            except Exception as e:
+                error_message(f"Serious error on running {assign_name}: {e}")
+                return -1
+        output = buffer1.getvalue()
+        if output == expected1:
+            print('✅', end='', flush=True)
+        elif output == expected2:
+            print('✅', end='', flush=True)
+        else:
+            print('❌', end='', flush=True)
+            stats += 1
+        if view:
+            print(f"\nEXPECTED 1:\n{expected1[i]}\nEXPECTED 2:\n{expected2[i]}\nOUTPUT:\n{output}")
+        i += 1
+
+    i = 0
+    buffer2 = StringIO()
+    with redirect_stdout(buffer2):
+        try:
+            for p in parameters:
+                plant = parameters[p]
+                plants = fclass(plant["name"], plant["height"], plant["age"], plant["grpd"])
+            try:
+                print(f"Total of plants: {plants.total_plants}")
+            except:
+                print("no class variable like total_plant")
+        except Exception as e:
+                error_message(f"Serious error on running {assign_name}: {e}")
+                return -1
+    output = buffer1.getvalue()
+    if output == expected3[0]:
+        print('✅', end='', flush=True)
+    elif output == expected3[1]:
+        print('✅', end='', flush=True)
+    else:
+        print('❌', end='', flush=True)
+        stats += 1
+    if view:
+        print(f"\nFINAL EXPECTED 1:\n{expected1[i]}\nFINAL EXPECTED 2:\n{expected2[i]}\nOUTPUT:\n{output}")
+        i += 1
+    return stats
 
 
 def exec_class(fclass, id: int, modules: dict, assign_name: str, parameters: dict, expected: dict, texts: dict, space_time: int = 1, view: bool=False, time: float=0.5) -> tuple[int, int]:
     i = 0
     stats = 0
     print(f"{CYAN}\n{assign_name}:{' ' * space_time}{DEFAULT}", end=('\n' if view else ''), flush=True)
-    for i, parameter in parameters.items():
-        tm.sleep(time)
-        buffer = StringIO()
-        try:
-            args = build_args(parameter)
-            output = exec_mod(fclass, id, modules, texts, args, assign_name)
-            if output == expected[i]:
-                print('✅', end='', flush=True)
-            else:
-                print('❌', end='', flush=True)
-                stats = 1
-            if view:
-                print(f"\nEXPECTED:\n{expected[i]}\nOUTPUT:\n{output}")
-        except Exception as e:
-            error_message(f"Serious error on running {assign_name}: {e}")
+    if i == 3:
+        ret = exec_special3(fclass, id, modules, assign_name, parameters, expected, view, time)
+        if ret == -1:
             return 1, 1
-        i += 1;
+        else:
+            return 0, stats
+    else:
+        for i, parameter in parameters.items():
+            tm.sleep(time)
+            try:
+                args = build_args(parameter)
+                output = exec_mod(fclass, id, modules, texts, args, assign_name)
+                if output == expected[i]:
+                    print('✅', end='', flush=True)
+                else:
+                    print('❌', end='', flush=True)
+                    stats = 1
+                if view:
+                    print(f"\nEXPECTED:\n{expected[i]}\nOUTPUT:\n{output}")
+            except Exception as e:
+                error_message(f"Serious error on running {assign_name}: {e}")
+                return 1, 1
+            i += 1;
     printnl()
     return 0, stats
 
@@ -156,10 +247,10 @@ def exec_tests(view: bool, time: float) -> int:
             "f2": "Plant Growth",
             "f3": "Plant Factory",
             "f4": "Garden Security",
-            "f5": "Plant Type: Flower",
-            "f6": "Plant Type: Tree",
-            "f7": "Plant Type: Vegetables",
-            "f8": "ft_seed_inventory",
+            # "f5": "Plant Type: Flower",
+            # "f6": "Plant Type: Tree",
+            # "f7": "Plant Type: Vegetables",
+            # "f8": "ft_seed_inventory",
         }
 
         funcs_params = {
@@ -167,10 +258,10 @@ def exec_tests(view: bool, time: float) -> int:
             "f1": list_of_plants0,
             "f2": list_of_plants1,
             "f3": list_of_plants1,
-            "f4": list_of_plants1,
-            "f5": list_of_flowers1,
-            "f6": list_of_trees,
-            "f7": list_of_vegetables,
+            # "f4": list_of_plants2,
+            # "f5": list_of_flowers1,
+            # "f6": list_of_trees,
+            # "f7": list_of_vegetables,
         }
 
         in_params0 = ["name", "height", "age"]
@@ -178,23 +269,23 @@ def exec_tests(view: bool, time: float) -> int:
         class_modules = {
             "f0": ["show"],
             "f1": ["show"],
-            "f2": ["show", "grow"],
-            "f3": ["show", "grow"],
-            "f4": ["show", "grow"],
-            "f5": ["show", "grow"],
-            "f6": ["show", "grow"],
-            "f7": ["show", "grow"],
+            "f2": ["show", "grow", "show"],
+            "f3": [None],
+            # "f4": ["show", "grow"],
+            # "f5": ["show", "grow"],
+            # "f6": ["show", "grow"],
+            # "f7": ["show", "grow"],
         }
 
         ret_expect = {
             "f0": f0e,
             "f1": f1e,
             "f2": f2e,
-            "f3": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
-            "f4": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
-            "f5": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
-            "f6": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
-            "f7": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            "f3": {1: f3e1, 2: f3e2, 3: f3e3},
+            # "f4": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            # "f5": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            # "f6": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
+            # "f7": {0: [], 1: [], 2: [], 3: [], 4: [], 5: []},
         }
 
     ret = 0
@@ -232,7 +323,7 @@ def exec_tests(view: bool, time: float) -> int:
         else:
             ret, stats = exec_class(to_run, i, modules, fun_name, params, params_expect, texts, space_times, view, time)
             checker += stats
-            if i == 2:
+            if i == 3:
                 break
         i += 1
     if ret == 0:
--- Milestone2/utils_m0.py
+++ Milestone2/utils_m0.py
@@ -6,7 +6,7 @@
 #    By: vigomes- <vigomes-@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/19 20:01:16 by vigomes-          #+#    #+#              #
-#    Updated: 2026/07/19 20:35:47 by vigomes-         ###   ########.fr        #
+#    Updated: 2026/07/20 20:51:05 by vigomes-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -44,6 +44,15 @@ list_of_plants0 = {
 list_of_plants1 = {
     0: {"name": "Rose", "height": 25, "plant_age": 30, "grpd": 0.86},
     1: {"name": "Sunflower", "height": 80, "plant_age": 45, "grpd": 0.9},
+    2: {"name": "Cactus", "height": 15, "plant_age": 120, "grpd": 0.15},
+    3: {"name": "Olive", "height": 150, "plant_age": 365, "grpd": 0.02},
+    4: {"name": "Venus", "height": 9, "plant_age": 1, "grpd": 0.5},
+    5: {"name": "whatever", "height": 0, "plant_age": 1, "grpd": 2.0},
+}
+
+list_of_plants2 = {
+    0: {"name": "Rose", "height": 25, "plant_age": 30, "grpd": 0.86},
+    1: {"name": "Sunflower", "height": 80, "plant_age": 45, "grpd": 0.9},
     2: {"name": "Cactus", "height": -15, "plant_age": 120, "grpd": 0.15},
     3: {"name": "Olive", "height": 150, "plant_age": -365, "grpd": 0.02},
     4: {"name": "Venus", "height": 9, "plant_age": 1, "grpd": 0.5},
@@ -62,7 +71,7 @@ TEXTS = {
     "f0": {"init_text": "=== Welcome to My Garden ===\n", "mid_text": None, "end_text": "=== End of Program ===\n"},
     "f1": {"init_text": "=== Garden Plant Registry ===\n", "mid_text": None, "end_text": None},
     "f2": {"init_text": "=== Day 1 ===\n", "mid_text": "=== Day 7 ===\n", "end_text": None},
-    "f3": {"init_text": "", "mid_text": "", "end_text": ""},
+    "f3": {"init_text": None, "mid_text": None, "end_text": None},
     "f4": {"init_text": "", "mid_text": "", "end_text": ""},
     "f5": {"init_text": "", "mid_text": "", "end_text": ""},
     "f6": {"init_text": "", "mid_text": "", "end_text": ""},
@@ -71,22 +80,22 @@ TEXTS = {
 
 MID_IDS = {
     0: -1,
-    1: 1,
-    2: 1,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
+    1: -1,
+    2: 2,
+    3: -1,
+    # 4: 4,
+    # 5: 5,
+    # 6: 6,
 }
 
 END_IDS = {
     0: 1,
     1: -1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
+    2: -1,
+    3: -1,
+    # 4: 4,
+    # 5: 5,
+    # 6: 6,
 }
 
 f0e = {
@@ -104,23 +113,37 @@ f1e = {
     5: "=== Garden Plant Registry ===\nwhatever: 0.0cm, 0 days old\n"
     }
 
-# f2e = {
-#     0: "=== Day 1 ===\nRose: 25.0cm, 30 days old\n=== Day 7 ===\nRose: 31.0cm, 37 days old\nGrowth this week: +6.02cm\n",
-#     1: "=== Day 1 ===\nSunflower: 80.0cm, 45 days old\n=== Day 7 ===\nSunflower: 86.3cm, 52 days old\nGrowth this week: +6.30cm\n",
-#     2: "Cactus: 15.0cm, 120 days old\n=== Day 7 ===\nCactus: 16.1cm, 127 days old\nGrowth this week: +1.05cm\n",
-#     3: "=== Day 1 ===\nOlive: 150.0cm, 365 days old\n=== Day 7 ===\nOlive: 150.1cm, 372 days old\nGrowth this week: +0.14cm\n",
-#     4: "=== Day 1 ===\nVenus: 9.0cm, 1 days old\n=== Day 7 ===\nVenus: 12.5cm, 8 days old\nGrowth this week: +3.50cm\n",
-#     5: "=== Day 1 ===\nwhatever: 0.0cm, 1 days old\n=== Day 7 ===\nGrowth this week: +14.00cm\n"
-#     }
-
 f2e = {
-    0: "=== Day 1 ===\nRose: 25.0cm, 30 days old\n=== Day 7 ===\nRose: 31.0cm, 37 days old\n",
-    1: "=== Day 1 ===\nSunflower: 80.0cm, 45 days old\n=== Day 7 ===\nSunflower: 86.3cm, 52 days old\n",
-    2: "Cactus: 15.0cm, 120 days old\n=== Day 7 ===\nCactus: 16.1cm, 127 days old\n",
-    3: "=== Day 1 ===\nOlive: 150.0cm, 365 days old\n=== Day 7 ===\nOlive: 150.1cm, 372 days old\n",
-    4: "=== Day 1 ===\nVenus: 9.0cm, 1 days old\n=== Day 7 ===\nVenus: 12.5cm, 8 days old\n",
-    5: "=== Day 1 ===\nwhatever: 0.0cm, 1 days old\n=== Day 7 ===\nwhatever: 14.0cm, 8 days old\n"
+    0: "=== Day 1 ===\nRose: 25.0cm, 30 days old\n=== Day 7 ===\nRose: 31.0cm, 37 days old\nGrowth this week: +6.02cm\n",
+    1: "=== Day 1 ===\nSunflower: 80.0cm, 45 days old\n=== Day 7 ===\nSunflower: 86.3cm, 52 days old\nGrowth this week: +6.30cm\n",
+    2: "=== Day 1 ===\nCactus: 15.0cm, 120 days old\n=== Day 7 ===\nCactus: 16.1cm, 127 days old\nGrowth this week: +1.05cm\n",
+    3: "=== Day 1 ===\nOlive: 150.0cm, 365 days old\n=== Day 7 ===\nOlive: 150.1cm, 372 days old\nGrowth this week: +0.14cm\n",
+    4: "=== Day 1 ===\nVenus: 9.0cm, 1 days old\n=== Day 7 ===\nVenus: 12.5cm, 8 days old\nGrowth this week: +3.50cm\n",
+    5: "=== Day 1 ===\nwhatever: 0.0cm, 1 days old\n=== Day 7 ===\nwhatever: 14.0cm, 8 days old\nGrowth this week: +14.00cm\n"
     }
+
+f3e1 = {
+    0: "Created:Rose: 25.0cm, 30 days old\n\nTotal plants: 1\n",
+    1: "Created:Sunflower: 80.0cm, 45 days old\n\nTotal plants: 1\n",
+    2: "Created:Cactus: 15.0cm, 120 days old\n\nTotal plants: 1\n",
+    3: "Created:Olive: 150.0cm, 365 days old\n\nTotal plants: 1\n",
+    4: "Created:Venus: 9.0cm, 1 days old\n\nTotal plants: 1\n",
+    5: "Created:whatever: 0.0cm, 1 days old\n\nTotal plants: 1\n",
+}
+
+f3e2 = {
+    0: "Created:Rose: 25.0cm, 30 days old\n\nno class variable like total_plant\n",
+    1: "Created:Sunflower: 80.0cm, 45 days old\n\nno class variable like total_plant\n",
+    2: "Created:Cactus: 15.0cm, 120 days old\n\nno class variable like total_plant\n",
+    3: "Created:Olive: 150.0cm, 365 days old\n\nno class variable like total_plant\n",
+    4: "Created:Venus: 9.0cm, 1 days old\n\nno class variable like total_plant\n",
+    5: "Created:whatever: 0.0cm, 1 days old\n\nno class variable like total_plant\n",
+}
+
+f3e3 = [
+    "Created: Rose: 25.0cm, 30 days old\nCreated: Sunflower: 80.0cm, 45 days old\nCreated: Cactus: 15.0cm, 120 days old\nCreated: Olive: 150.0cm, 365 days old\nCreated: Venus: 9.0cm, 1 days old\nCreated: whatever: 0.0cm, 1 days old\nTotal plants: 6\n",
+    "Created: Rose: 25.0cm, 30 days old\nCreated: Sunflower: 80.0cm, 45 days old\nCreated: Cactus: 15.0cm, 120 days old\nCreated: Olive: 150.0cm, 365 days old\nCreated: Venus: 9.0cm, 1 days old\nCreated: whatever: 0.0cm, 1 days old\nno class variable like total_plant\n"
+    ]
 
 
 def clear() -> None:
